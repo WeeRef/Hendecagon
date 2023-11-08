5a05d61c8d4bd06b7fef81f28cb6ca1a2cc9ca45
Supawit.P <catthailand009@gmail.com>
2023-11-09 04:53:15 +0700

    first commit

---
+++ 2.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("sl");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+      
+    }
+    dots[slideIndex-1].className += " active";
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 45000);
+  if (slideIndex<3){slideIndex++}
+  else{slideIndex=1}
+
+  }
